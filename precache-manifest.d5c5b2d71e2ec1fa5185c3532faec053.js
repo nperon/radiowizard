4fc9fc6390fd6b29f229a3d2b166d49ea3d80259
nperon <nperon@gmail.com>
2019-12-08 23:05:06 +0100

    Updates

---
+++ precache-manifest.d5c5b2d71e2ec1fa5185c3532faec053.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02d76a73edff6deba88f8681ac747e06",
+    "url": "/radiowizard/index.html"
+  },
+  {
+    "revision": "9b1a43ff03fe5dcd28b7",
+    "url": "/radiowizard/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0f2d0f29bfcddd726058",
+    "url": "/radiowizard/static/js/2.1b11ea37.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/radiowizard/static/js/2.1b11ea37.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9b1a43ff03fe5dcd28b7",
+    "url": "/radiowizard/static/js/main.3928de4f.chunk.js"
+  },
+  {
+    "revision": "eb91bd126810b3effd4a",
+    "url": "/radiowizard/static/js/runtime-main.341808fa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/radiowizard/static/media/logo.5d5d9eef.svg"
+  }
+]);
