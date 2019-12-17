86a46e38b9bfe37c45a7bc51a4fd0660c37408da
Nicolas Peron <nperon@giesiis.fr>
2019-12-17 14:59:34 +0100

    search middleware initiated

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 import './style/style.css';
 import reducers from './reducers';
 import App from './App';
+import { Types, updateFeaturedAction } from './actions/featured';
 
-const store = createStore( reducers );
+const searchMiddleware = store => next => action => {
+    console.log('in middleware ', action.type);
+    if ( action.type === Types.SEARCH_FOR_TEXT ) {
+        next(updateFeaturedAction([0,1,2]));
+        return;
+    }
+    next(action);
+    return;
+}
+
+const store = createStore(
+    reducers,
+    applyMiddleware(searchMiddleware)
+);
 
 ReactDOM.render(
     <Provider store={store}>
